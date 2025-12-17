38b3fe939f16d6885b8461932cdcd9f407432942
Crowther <techcrowther@gmail.com>
2025-12-17 16:41:51 +0100

    Enable firstRun scheduler in index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ require('./interactions/mentions');
 // require('./interactions/dmChat'); // Disabled on Free plan (DMs require elevated access)
 
 // First deploy post
-//const firstRun = require('./scheduler/firstRun');
+const firstRun = require('./scheduler/firstRun');
 firstRun();
 
 console.log("Dev Fun Bot is running...");
